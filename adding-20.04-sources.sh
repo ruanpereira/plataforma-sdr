8092ff0807b49abf51d34d8b696382428e15248d
black_aktsu <black_aktsu@proton.me>
2024-05-14 15:16:14 -0300

    adding sources.list script

---
+++ adding-20.04-sources.sh
@@ -0,0 +1,22 @@
+!#/usr/bin
+
+echo "
+
+Ubuntu 20.04 sources list
+
+deb http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse 
+deb-src http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse 
+ 
+deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse 
+deb-src http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse 
+ 
+deb http://archive.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse 
+deb-src http://archive.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse 
+ 
+deb http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse 
+deb-src http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse 
+ 
+deb http://archive.canonical.com/ubuntu focal partner 
+deb-src http://archive.canonical.com/ubuntu focal partner" >> /etc/apt/sources.list
+
+sudo apt update && sudo apt upgrade
